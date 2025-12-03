39110e1f2ed65cca6198a205cf5bcb6b06336f4f
C6002536 <mahendra.chaurasia@conduent.com>
2025-12-03 15:44:54 +0530

    add rev2 and countNum

--- src/main/java/org/mindae/arrays.java
+++ src/main/java/org/mindae/arrays.java
@@ -1,13 +1,27 @@
 package org.mindae;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class arrays {
     public static void main(String[] args) {
-        int arr[] = {4, 9, 2, 3, 11};
+        int arr[] = {4, 9, 2, 3, 11, 2, 2};
 //        max(arr);
 //        rev(arr);
         rev2(arr);
+//        countNum(arr, 2);
+    }
+
+    public static void countNum(int[] arr, int num) {
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == num) {
+                count++;
+            }
+        }
+        System.out.println(count);
     }
 
     public static void max(int arr[]) {
@@ -37,16 +51,17 @@ public class arrays {
             System.out.print(arr2[i] + " ");
         }
     }
-    public static void rev2(int[] arr){
-        int[] arr2=Arrays.copyOf(arr, arr.length);
+
+    public static void rev2(int[] arr) {
+        int[] arr2 = Arrays.copyOf(arr, arr.length);
         for (int i = 0; i < arr2.length; i++) {
             System.out.print(arr2[i] + " ");
         }
-        int left=0, right=arr2.length-1;
-        while(left<right){
-            int t=arr2[left];
-            arr2[left++]=arr2[right];
-            arr2[right--]=t;
+        int left = 0, right = arr2.length - 1;
+        while (left < right) {
+            int t = arr2[left];
+            arr2[left++] = arr2[right];
+            arr2[right--] = t;
         }
         System.out.println();
         for (int i = 0; i < arr2.length; i++) {
@@ -54,3 +69,4 @@ public class arrays {
         }
     }
 }
+
